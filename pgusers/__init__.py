bbf67f3caa05463aca68062c62137c29f844573e
Javier Llopis <javier@llopis.me>
2022-04-06 00:01:27 +0100

    Add README.rst, fix requirements

--- pgusers/__init__.py
+++ pgusers/__init__.py
@@ -1,6 +1,6 @@
 from .pgusers import BadCallError, UserSpace, OK, NOT_FOUND, EXPIRED, REJECTED
 
-__version__ = (0, 1, 5)
+__version__ = (0, 9, 1)
 version = "{0}.{1}.{2}".format(*__version__)
 
 __all__ = [
